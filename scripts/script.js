6c88064351ecf3e57a258ac246603853dd0b57fd
jjcasco99 <juanjocasco28@gmail.com>
2021-11-04 16:03:31 +0100

    app post de registro usuario

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,23 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/insertar', urlencodedParser, (req, res) => {
+    MongoClient.connect(url+mydb, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(mydb);
+        let nuevoUsuario = {"Nombre": req.body.nombrePersona,
+                    "Direccion": req.body.direccionPersona, 
+                    "Telefono": req.body.telefonoPersona,
+                    "Email": req.body.emailPersona,
+                    "DNI": req.body.dniPersona,
+                    "Contraseña": req.body.contraPerosna,
+                    "BitCoins": req.body.cantidadCompra};
+        dbo.collection(clientes).insertOne(nuevoUsuario, function(err, res) {
+          if (err) throw err;
+          console.log("Documento insertado");
+          db.close();
+        });
+      });
+    res.send(req.body);
+});
